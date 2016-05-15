45daf953f395734875d689799d00e00bb71ae77b
chenLX <783243077@qq.com>
2016-05-16 01:09:16 +0800

    添加用户数据库模型，前端登陆界面，添加加密模块，修改grunt监视目录

---
+++ schemas/user.js
@@ -0,0 +1,63 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var SALT_WORL_FACTOR = 10;
+var MovieSchema = new mongoose.Schema({
+    name: {
+        unique: true,
+        type: String
+    },
+
+    password: String,
+
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+
+});
+
+
+MovieSchema.pre('save', function (next) {
+
+    var user = this;
+
+    if (this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now();
+    } else {
+        this.meta.updateAt = Date.now();
+    }
+
+    bctypt.genSalt(SALT_WORL_FACTOR, function (err, salt) {
+        if (err) { return next(err) }
+        bctypt.hash(user.password, salt, function (err, hash) {
+            if (err) { return next(err) }
+
+            user.password = hash
+            next()
+        })
+    })
+
+    next();
+});
+
+MovieSchema.statics = {
+    fetch: function (cb) {
+        return this
+            .find({})
+            .sort('meta.updateAt')
+            .exec(cb);
+    },
+    findById: function (id, cb) {
+        return this
+            .findOne({_id: id})
+            .exec(cb);
+    }
+};
+
+module.exports = MovieSchema;
